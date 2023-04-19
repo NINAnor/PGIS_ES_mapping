8d8bf8c146b154c74140dd64643596f517ca8c68
Reto Spielhofer <reto.spielhofer@nina.no>
2023-04-19 14:39:50 +0200

    initial page with email

--- app_modu/ui.R
+++ app_modu/ui.R
@@ -1,13 +1,24 @@
 
 navbarPage('POC PGIS ES
                  ',id = "inTabset",
-                 tabPanel(title = "About you", value = "p1",
+           tabPanel(title = "About the study", value = "p0",
+                    mainPanel(h5("This is the study description, goals, aims, why we need you"),
+                              h5("The study consists of two rounds ... if you want to participate in both rounds please fill in zour e-mail, if not you can only participate in R1"),
+                              textInput("email",
+                                        label = tagList(shiny::icon("envelope"), "Email"),
+                                        value = "",
+                                        width = NULL,
+                                        placeholder = NULL),
+                               actionButton('sub0', 'start'))
+                   
+           ),
+          tabPanel(title = "About you", value = "p1",
                           return_quest_UI("return_quest"),
                           selectModUI("map_living"),
                           actionButton('sub1', 'submit answers')
                           ),
            
-           tabPanel(title = paste0("Mapping of ",sel_es_full), value = "p2", 
+           tabPanel(title = "Mapping of ecosystem services", value = "p2", 
                     mainPanel(
                       ESmoduleUI("es_quest"),
                       editModUI("map"),
@@ -25,9 +36,10 @@ navbarPage('POC PGIS ES
            #          DTOutput("tbl"),
            #          actionButton('sub3', 'calculate extrapolation'),),
            # 
-           tabPanel(title = paste0("your extrapolated map of ",sel_es_full), value = "p3",
+           tabPanel(title = paste0("your extrapolated map of "), value = "p3",
                       # geeUI("map_extra")
-                      leafletOutput(outputId = "gee_map")%>% withSpinner(color="#0dc5c1")
+                      leafletOutput(outputId = "gee_map")%>% withSpinner(color="#0dc5c1"),
+                      textOutput("n_img")
                     )
 )
            
