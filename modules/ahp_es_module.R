ce43cd20045dc082dde692d7326adfa8331cde7b
Reto Spielhofer <reto.spielhofer@nina.no>
2023-07-31 08:38:41 +0200

    sliders, texts and links in module done

--- modules/ahp_es_module.R
+++ modules/ahp_es_module.R
@@ -1,6 +1,7 @@
 library(shinyWidgets)
 library(dplyr)
 library(shinyBS)
+library(bigrquery)
 
 bq_auth(path = "C:/Users/reto.spielhofer/OneDrive - NINA/Documents/Projects/WENDY/rgee-381312-85272383f82d.json")
 # connection to bq
@@ -21,19 +22,34 @@ ahpUI<- function(id, label = "ahp2") {
       h6("Comparison of single ecosystem services"),
       br(),
       "You will now compare the importance of different ecosystem services within the 
-         study region. If you need more information about a specific benefit please click on the respective name.",
+         study region. Click on the name of the respective ecosysystem service, if you need further information.",
       br(),
       br(),
-      # textOutput(ns("expl1")),
-      uiOutput(ns("slider_es1")),
+      fluidRow(
+        column(3),
+        column(8,
+               uiOutput(ns("slider_es1"))
+               ),
+        column(1)
+      ),
       br(),
       br(),
-      # textOutput(ns("expl2")),
-      uiOutput(ns("slider_es2")),
+      fluidRow(
+        column(3),
+        column(8,
+               uiOutput(ns("slider_es2"))
+        ),
+        column(1)
+      ),
       br(),
       br(),
-      # textOutput(ns("expl3")),
-      uiOutput(ns("slider_es3")),
+      fluidRow(
+        column(3),
+        column(8,
+               uiOutput(ns("slider_es3"))
+        ),
+        column(1)
+      ),
       
       actionButton(ns("conf3"), "Next task", class='btn-primary')
     )
@@ -54,12 +70,14 @@ ahpServer<-function(id, userID, siteID, es_all){
       reg_comb<-as.data.frame(t(combn(reg, 2)))
       reg_comb$ind<-rep(1,nrow(reg_comb))
       
-      cul<-es_all%>%filter(esSECTION == "cultural")%>%distinct(esID)
+      cul_full<-es_all%>%filter(esSECTION == "cultural")
+      cul<-cul_full%>%distinct(esID)
       cul <- unlist(as.vector(cul))
       cul_comb<-as.data.frame(t(combn(cul, 2)))
       cul_comb$ind<-rep(2,nrow(cul_comb))
       
-      prov<-es_all%>%filter(esSECTION == "provisioning")%>%distinct(esID)
+      prov_full<-es_all%>%filter(esSECTION == "provisioning")
+      prov<-prov_full%>%distinct(esID)
       prov <- unlist(as.vector(prov))
       prov_comb<-as.data.frame(t(combn(prov, 2)))
       prov_comb$ind<-rep(3,nrow(prov_comb))
@@ -74,37 +92,22 @@ ahpServer<-function(id, userID, siteID, es_all){
       
       ## first ES block
       output$slider_es1 <- shiny::renderUI({
-        
-        
-        ns <- session$ns
-        
-          # lapply(1:nrow(es1),function(a){
-          #   es_id_left<-es1[a,]$V1
-          #   actionLink(inputId = ns(paste0("l_",es_id_left)), label = es_id_left)
-          # 
-          # }),
-          # lapply(1:nrow(es1),function(b){
-          #   es_id_left<-es1[b,]$V1
-             # bsModal(id = "modal1", title = es_id_left, trigger = ns(paste0("l_",es_id_left)),
-             #        h5(es_id_left),
-             #        tags$ol(
-             #          tags$li("Must have Resp_ID as the first column, occasion_ID as second and dependent variable as the third"),
-             #          tags$li("Must have no missing value in any fields")
-             #        ), easyClose = TRUE, footer = NULL)
-          # }),
-          lapply(1:nrow(es1),function(n){
+
+        # ns <- session$ns
+        lapply(1:nrow(es1),function(n){
             
             pair_id <- paste0(es1[n,]$V1,"_",es1[n,]$V2)
-            pair_lable<-paste0(reg_full%>%filter(esID %in% es1[n,]$V1)%>%select(esNAME)," - ",reg_full%>%filter(esID %in% es1[n,]$V2)%>%select(esNAME))
-            choice1<-paste0(es1[n,]$V1, " is overwhelmingly more important")
-            choice2<-paste0(es1[n,]$V1, " is very strongly more important")
-            choice3<-paste0(es1[n,]$V1, " is strongly more important")
-            choice4<-paste0(es1[n,]$V1, " is moderately more important")
+            # pair_lable<-paste0(reg_full%>%filter(esID %in% es1[n,]$V1)%>%select(esNAME)," - ",reg_full%>%filter(esID %in% es1[n,]$V2)%>%select(esNAME))
+            pair_lable<-""
+            choice1<-paste0(reg_full%>%filter(esID %in% es1[n,]$V1)%>%select(esNAME), " is overwhelmingly more important")
+            choice2<-paste0(reg_full%>%filter(esID %in% es1[n,]$V1)%>%select(esNAME), " is very strongly more important")
+            choice3<-paste0(reg_full%>%filter(esID %in% es1[n,]$V1)%>%select(esNAME), " is strongly more important")
+            choice4<-paste0(reg_full%>%filter(esID %in% es1[n,]$V1)%>%select(esNAME), " is moderately more important")
             
-            choice5<-paste0(es1[n,]$V2, " is overwhelmingly more important")
-            choice6<-paste0(es1[n,]$V2, " is very strongly more important")
-            choice7<-paste0(es1[n,]$V2, " is strongly more important")
-            choice8<-paste0(es1[n,]$V2, " is moderately more important")
+            choice5<-paste0(reg_full%>%filter(esID %in% es1[n,]$V2)%>%select(esNAME), " is overwhelmingly more important")
+            choice6<-paste0(reg_full%>%filter(esID %in% es1[n,]$V2)%>%select(esNAME), " is very strongly more important")
+            choice7<-paste0(reg_full%>%filter(esID %in% es1[n,]$V2)%>%select(esNAME), " is strongly more important")
+            choice8<-paste0(reg_full%>%filter(esID %in% es1[n,]$V2)%>%select(esNAME), " is moderately more important")
             
             
             
@@ -146,9 +149,8 @@ ahpServer<-function(id, userID, siteID, es_all){
                       ), easyClose = TRUE, footer = NULL),
 
               
-              column(6,actionLink(inputId = ns(paste0("l_",es_id_left)), label = es_id_left))
-            , 
-            column(6, actionLink(inputId = ns(paste0("r_",es_id_right)), label = es_id_right)),
+            column(6,actionLink(inputId = ns(paste0("l_",es_id_left)), label = reg_full%>%filter(esID %in% es1[n,]$V1)%>%select(esNAME))),
+            column(6, actionLink(inputId = ns(paste0("r_",es_id_right)), label = reg_full%>%filter(esID %in% es1[n,]$V2)%>%select(esNAME))),
             
             sliderTextInput(ns(pair_id),
                             pair_lable, 
@@ -157,86 +159,152 @@ ahpServer<-function(id, userID, siteID, es_all){
                             choices = c(choice1, 
                                         choice2,choice3,choice4, "both are equally important",choice8,choice7,choice6, choice5),
                             width = "75%"
-                            
-            ))
-            
-            
-          })
-          
-        
-        
-      })
+            ))#/slider
+          })#/lapply
+      })#/ui es 1
       
       ## second ES block
       output$slider_es2 <- shiny::renderUI({
-        ns <- session$ns
-        tagList(
-          lapply(1:nrow(es2),function(m){
-            pair_id2 <- paste0(es2[m,]$V1,"_",es2[m,]$V2)
-            pair_lable2<-paste0(es2[m,]$V1," - ",es2[m,]$V2)
-            choice1<-paste0(es2[m,]$V1, " is overwhelmingly more important")
-            choice2<-paste0(es2[m,]$V1, " is very strongly more important")
-            choice3<-paste0(es2[m,]$V1, " is strongly more important")
-            choice4<-paste0(es2[m,]$V1, " is moderately more important")
+        # ns <- session$ns
+        lapply(1:nrow(es2),function(m){
+          
+          pair_id <- paste0(es2[m,]$V1,"_",es2[m,]$V2)
+          # pair_lable<-paste0(reg_full%>%filter(esID %in% es2[n,]$V1)%>%select(esNAME)," - ",reg_full%>%filter(esID %in% es2[n,]$V2)%>%select(esNAME))
+          pair_lable<-""
+          choice1<-paste0(cul_full%>%filter(esID %in% es2[m,]$V1)%>%select(esNAME), " is overwhelmingly more important")
+          choice2<-paste0(cul_full%>%filter(esID %in% es2[m,]$V1)%>%select(esNAME), " is very strongly more important")
+          choice3<-paste0(cul_full%>%filter(esID %in% es2[m,]$V1)%>%select(esNAME), " is strongly more important")
+          choice4<-paste0(cul_full%>%filter(esID %in% es2[m,]$V1)%>%select(esNAME), " is moderately more important")
+          
+          choice5<-paste0(cul_full%>%filter(esID %in% es2[m,]$V2)%>%select(esNAME), " is overwhelmingly more important")
+          choice6<-paste0(cul_full%>%filter(esID %in% es2[m,]$V2)%>%select(esNAME), " is very strongly more important")
+          choice7<-paste0(cul_full%>%filter(esID %in% es2[m,]$V2)%>%select(esNAME), " is strongly more important")
+          choice8<-paste0(cul_full%>%filter(esID %in% es2[m,]$V2)%>%select(esNAME), " is moderately more important")
+          
+          
+          
+          es_id_left<-es2[m,]$V1
+          es_id_right<-es2[m,]$V2
+          mod_id_left<-paste0(es_id_left,"_mod_left")
+          mod_id_right<-paste0(es_id_right,"_mod_right")
+          
+          
+          tagList(
+            bsModal(id = ns(paste0(mod_id_left,m)), title = cul_full%>%filter(esID %in% es2[m,]$V1)%>%select(esNAME), trigger = ns(paste0("l_",es_id_left)),
+                    h6(es_id_left),
+                    tags$ol(
+                      tags$li(cul_full%>%filter(esID %in% es2[m,]$V1)%>%select(esDESCR)),
+                      tags$figure(
+                        class = "centerFigure",
+                        tags$img(
+                          src = paste0(es2[m,]$V1,".jpg"),
+                          width = 300,
+                          alt = "Picture of an astragalus (bone die)"
+                        ),
+                        tags$figcaption("Image of Astragalus by Yaan, 2007")
+                      )
+                    ), easyClose = TRUE, footer = NULL),
             
-            choice5<-paste0(es2[m,]$V2, " is overwhelmingly more important")
-            choice6<-paste0(es2[m,]$V2, " is very strongly more important")
-            choice7<-paste0(es2[m,]$V2, " is strongly more important")
-            choice8<-paste0(es2[m,]$V2, " is moderately more important")
+            bsModal(id = ns(paste0(mod_id_right,m)), title = cul_full%>%filter(esID %in% es2[m,]$V2)%>%select(esNAME), trigger = ns(paste0("r_",es_id_right)),
+                    h6(es_id_right),
+                    tags$ol(
+                      tags$li(cul_full%>%filter(esID %in% es2[m,]$V1)%>%select(esDESCR)),
+                      tags$figure(
+                        class = "centerFigure",
+                        tags$img(
+                          src = paste0(es2[m,]$V2,".jpg"),
+                          width = 300,
+                          alt = "Picture of an astragalus (bone die)"
+                        ),
+                        tags$figcaption("Image of Astragalus by Yaan, 2007")
+                      )
+                    ), easyClose = TRUE, footer = NULL),
             
             
+            column(6,actionLink(inputId = ns(paste0("l_",es_id_left)), label = cul_full%>%filter(esID %in% es2[m,]$V1)%>%select(esNAME))),
+            column(6, actionLink(inputId = ns(paste0("r_",es_id_right)), label = cul_full%>%filter(esID %in% es2[m,]$V2)%>%select(esNAME))),
             
-            sliderTextInput(ns(pair_id2),
-                            pair_lable2, 
+            sliderTextInput(ns(pair_id),
+                            pair_lable, 
                             grid = F,
-                            force_edges = TRUE,
+                            force_edges = F,
                             choices = c(choice1, 
                                         choice2,choice3,choice4, "both are equally important",choice8,choice7,choice6, choice5),
                             width = "75%"
-                            
-            )
-            
-            
-          })
-          
-        )
+            ))#/slider
+        })#/lapply
         
-      })
+      })#/ui
       
       ## third ES block
       output$slider_es3 <- shiny::renderUI({
-        ns <- session$ns
-        tagList(
-          lapply(1:nrow(es3),function(o){
-            pair_id3 <- paste0(es3[o,]$V1,"_",es3[o,]$V2)
-            pair_lable3<-paste0(es3[o,]$V1," - ",es3[o,]$V2)
-            choice1<-paste0(es3[o,]$V1, " is overwhelmingly more important")
-            choice2<-paste0(es3[o,]$V1, " is very strongly more important")
-            choice3<-paste0(es3[o,]$V1, " is strongly more important")
-            choice4<-paste0(es3[o,]$V1, " is moderately more important")
+        lapply(1:nrow(es3),function(o){
+          
+          pair_id <- paste0(es3[o,]$V1,"_",es3[o,]$V2)
+          # pair_lable<-paste0(reg_full%>%filter(esID %in% es3[n,]$V1)%>%select(esNAME)," - ",reg_full%>%filter(esID %in% es3[n,]$V2)%>%select(esNAME))
+          pair_lable<-""
+          choice1<-paste0(prov_full%>%filter(esID %in% es3[o,]$V1)%>%select(esNAME), " is overwhelmingly more important")
+          choice2<-paste0(prov_full%>%filter(esID %in% es3[o,]$V1)%>%select(esNAME), " is very strongly more important")
+          choice3<-paste0(prov_full%>%filter(esID %in% es3[o,]$V1)%>%select(esNAME), " is strongly more important")
+          choice4<-paste0(prov_full%>%filter(esID %in% es3[o,]$V1)%>%select(esNAME), " is moderately more important")
+          
+          choice5<-paste0(prov_full%>%filter(esID %in% es3[o,]$V2)%>%select(esNAME), " is overwhelmingly more important")
+          choice6<-paste0(prov_full%>%filter(esID %in% es3[o,]$V2)%>%select(esNAME), " is very strongly more important")
+          choice7<-paste0(prov_full%>%filter(esID %in% es3[o,]$V2)%>%select(esNAME), " is strongly more important")
+          choice8<-paste0(prov_full%>%filter(esID %in% es3[o,]$V2)%>%select(esNAME), " is moderately more important")
+          
+          
+          
+          es_id_left<-es3[o,]$V1
+          es_id_right<-es3[o,]$V2
+          mod_id_left<-paste0(es_id_left,"_mod_left")
+          mod_id_right<-paste0(es_id_right,"_mod_right")
+          
+          
+          tagList(
+            bsModal(id = ns(paste0(mod_id_left,o)), title = prov_full%>%filter(esID %in% es3[o,]$V1)%>%select(esNAME), trigger = ns(paste0("l_",es_id_left)),
+                    h6(es_id_left),
+                    tags$ol(
+                      tags$li(prov_full%>%filter(esID %in% es3[o,]$V1)%>%select(esDESCR)),
+                      tags$figure(
+                        class = "centerFigure",
+                        tags$img(
+                          src = paste0(es3[o,]$V1,".jpg"),
+                          width = 300,
+                          alt = "Picture of an astragalus (bone die)"
+                        ),
+                        tags$figcaption("Image of Astragalus by Yaan, 2007")
+                      )
+                    ), easyClose = TRUE, footer = NULL),
             
-            choice5<-paste0(es3[o,]$V2, " is overwhelmingly more important")
-            choice6<-paste0(es3[o,]$V2, " is very strongly more important")
-            choice7<-paste0(es3[o,]$V2, " is strongly more important")
-            choice8<-paste0(es3[o,]$V2, " is moderately more important")
+            bsModal(id = ns(paste0(mod_id_right,o)), title = prov_full%>%filter(esID %in% es3[o,]$V2)%>%select(esNAME), trigger = ns(paste0("r_",es_id_right)),
+                    h6(es_id_right),
+                    tags$ol(
+                      tags$li(prov_full%>%filter(esID %in% es3[o,]$V1)%>%select(esDESCR)),
+                      tags$figure(
+                        class = "centerFigure",
+                        tags$img(
+                          src = paste0(es3[o,]$V2,".jpg"),
+                          width = 300,
+                          alt = "Picture of an astragalus (bone die)"
+                        ),
+                        tags$figcaption("Image of Astragalus by Yaan, 2007")
+                      )
+                    ), easyClose = TRUE, footer = NULL),
             
             
+            column(6, actionLink(inputId = ns(paste0("l_",es_id_left)), label = prov_full%>%filter(esID %in% es3[o,]$V1)%>%select(esNAME))),
+            column(6, actionLink(inputId = ns(paste0("r_",es_id_right)), label = prov_full%>%filter(esID %in% es3[o,]$V2)%>%select(esNAME))),
             
-            sliderTextInput(ns(pair_id3),
-                            pair_lable3, 
+            sliderTextInput(ns(pair_id),
+                            pair_lable, 
                             grid = F,
-                            force_edges = TRUE,
+                            force_edges = F,
                             choices = c(choice1, 
                                         choice2,choice3,choice4, "both are equally important",choice8,choice7,choice6, choice5),
                             width = "75%"
-                            
-            )
-            
-            
-          })
-          
-        )
-        
+            ))#/slider
+        })#/lapply
       })      
       
       
